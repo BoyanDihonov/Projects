504e1ae8ac79e1c281b274311b0574f837551d8b
Boyan Dihonov <boyandihonov@abv.bg>
2024-08-20 14:18:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+// Generate a random number for Player 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// Select the first dice image and update its src attribute
+document.querySelector('.img1').setAttribute("src", `./images/dice${randomNumber1}.png`);
 
-var randomNumber1 = Math.floor(Math.random() * 6 + 1)
-
+// Generate a random number for Player 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+// Select the second dice image and update its src attribute
+document.querySelector('.img2').setAttribute("src", `./images/dice${randomNumber2}.png`);
