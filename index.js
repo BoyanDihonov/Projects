25d27bad0665de32ef8cc34ea9319120e00c42a1
Boyan Dihonov <boyandihonov@abv.bg>
2024-08-20 14:37:00 +0300

    finished the css
    added a button for rolling the dice

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ document.querySelector('.img1').setAttribute("src", `./images/dice${randomNumber
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 // Select the second dice image and update its src attribute
 document.querySelector('.img2').setAttribute("src", `./images/dice${randomNumber2}.png`);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = "🚩 Player 1 Wins"
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector('h1').innerHTML = "Player 2 wins 🚩"
+} else {
+    document.querySelector('h1').innerHTML = "It's a Draw!"
+}
+
+
+function rollDice(){
+    // Generate a random number for Player 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// Select the first dice image and update its src attribute
+document.querySelector('.img1').setAttribute("src", `./images/dice${randomNumber1}.png`);
+
+// Generate a random number for Player 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+// Select the second dice image and update its src attribute
+document.querySelector('.img2').setAttribute("src", `./images/dice${randomNumber2}.png`);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = "🚩 Player 1 Wins"
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector('h1').innerHTML = "Player 2 wins 🚩"
+} else {
+    document.querySelector('h1').innerHTML = "It's a Draw!"
+}
+}
