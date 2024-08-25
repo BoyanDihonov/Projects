a7bf0f564914184b1a7b666eafb3a74b03d0df80
Boyan Dihonov <boyandihonov@abv.bg>
2024-08-25 11:09:33 +0300

    Update index.js

--- drumKit/index.js
+++ drumKit/index.js
@@ -1,48 +1,52 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i <= numberOfDrumButtons; i++) {
+for (var i = 0; i < numberOfDrumButtons; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
         var buttonInnerHTML = this.innerHTML;
 
-        switch (buttonInnerHTML) {
-            case "w":
-                var tom1 = new Audio('./sounds/tom-1.mp3');
-                tom1.play();
-                break;
-            case "a":
-                var tom2 = new Audio('./sounds/tom-2.mp3');
-                tom2.play();
-                break;
-            case "s":
-                var tom3 = new Audio('./sounds/tom-3.mp3');
-                tom3.play();
-                break;
-            case "d":
-                var tom4 = new Audio('./sounds/tom-4.mp3');
-                tom4.play();
-                break;
-            case "j":
-                var snare = new Audio('./sounds/snare.mp3');
-                snare.play();
-                break;
-            case "k":
-                var crash = new Audio('./sounds/crash.mp3');
-                crash.play();
-                break;
-            case "l":
-                var kickbass = new Audio('./sounds/kick-bass.mp3');
-                kickbass.play();
-                break;
-        }
+        makeSound(buttonInnerHTML); // Предаваме buttonInnerHTML като аргумент
 
+    });
+}
 
-        // var audio = new Audio('./sounds/tom-1.mp3');
-        // audio.play();
-        // this.style.color = "white"
-        // setTimeout(() => {
-        //     this.style.color = "#DA0463";
-        // }, 100)
-    })
+// Добавяме слушател за натискане на клавиши извън цикъла
+document.addEventListener("keydown", function (e) {
+    makeSound(e.key);
+});
+
+function makeSound(key) {
+    switch (key) { // Променяме buttonInnerHTML на key
+        case "w":
+            var tom1 = new Audio('./sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio('./sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio('./sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio('./sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio('./sounds/snare.mp3');
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio('./sounds/crash.mp3');
+            crash.play();
+            break;
+        case "l":
+            var kickbass = new Audio('./sounds/kick-bass.mp3');
+            kickbass.play();
+            break;
+        default:
+            alert("Invalid key pressed: " + key);
+    }
 }
