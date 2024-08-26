7e8ce17b1afc394330fdb02c0b3ec4540534c738
Boyan Dihonov <boyandihonov@abv.bg>
2024-08-26 15:54:35 +0300

    Update game.js

--- Simon Game Challange/game.js
+++ Simon Game Challange/game.js
@@ -1,10 +1,29 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
 
     gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
 }
+
+
+$(".btn").click(function() {
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+
+    // console.log(userClickedPattern);
+
+    var audio = new Audio("sounds/" + userChosenColour + ".mp3");
+    audio.play();
+
+    
+});
+
 
